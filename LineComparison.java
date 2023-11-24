2dc811e230aa1609e7077e9c8c14a3d424887ebb
Dipti-Deshmane <itsmedipu17@gmail.com>
2023-11-24 12:20:57 +0530

    UC1 added

---
+++ LineComparison.java
@@ -0,0 +1,24 @@
+package LineComparison;
+
+import java.util.Scanner;
+
+public class LineComparison {
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter the coordinates of the first point x1, y1: ");
+    double x1 = scanner.nextDouble();
+    double y1 = scanner.nextDouble();
+
+    System.out.print("Enter the coordinates of the second point x2, y2: ");
+    double x2 = scanner.nextDouble();
+    double y2 = scanner.nextDouble();
+
+
+    double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+    System.out.println("Length of the line = " + length);
+}
+
+}
